c3bcf4e4a5548b9aa4958ca2cb74468903179aea
arun kumar <arunkumarpostbox2369@gmail.com>
2025-05-02 00:24:49 +0530

    Again starting

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-const heading = React.createElement("h1",{id:"hello"},"hello world from react")
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading);
+// React element
+// jsx is react element
 
+const Title = () => (
+  // react component
+  <h1 className="root"> Namste raect from Arun</h1>
+);
+
+// React Component
+// raect component is return jsx
+
+const Headingcomponent = () => (
+  <div id="container">
+    <Title />
+    <h1>Namste React functional component </h1>;
+  </div>
+);
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<Headingcomponent />);
+
+// Create a Nested header Element using React.createElement(h1,h2,h3 inside a
+//div with class “title”)
+
+const element = React.createElement(
+  "div" ,
+   {className:"title"} , // div with class "title"
+  React.createElement("h1",null,"headong1"),
+  React.createElement("h2",null,"headong2"),
+  React.createElement("h3",null,"headong3")
+);
+const root =ReactDOM.createElement(document.getElementById("root"));
+root.render(element);
---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id: "hello"
+}, "hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
